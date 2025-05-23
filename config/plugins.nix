2c21ed536bbe028b526aefa17fbb8c8385d8b16b
repparw <ubritos@gmail.com>
2025-05-22 21:26:07 -0300

    avante: model change to sonnet-4

--- config/plugins.nix
+++ config/plugins.nix
@@ -32,7 +32,7 @@
           enable_cursor_planning_mode = true;
         };
         copilot = {
-          model = "claude-3.5-sonnet";
+          model = "claude-sonnet-4";
         };
       };
       luaConfig.post = ''
