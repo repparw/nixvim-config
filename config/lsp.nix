a6fbccf99ea39d8e60ff306b3b3ca0fefe665ccd
repparw <ubritos@gmail.com>
2026-03-10 17:34:16 -0300

    feat(lsp): terraform

--- config/lsp.nix
+++ config/lsp.nix
@@ -4,13 +4,14 @@
     enable = true;
     inlayHints = true;
     servers = {
-      lua_ls.enable = true;
-      nixd.enable = true;
-      ts_ls.enable = true;
-      marksman.enable = true;
-      pyright.enable = true;
-      jsonls.enable = true;
       jdtls.enable = true;
+      jsonls.enable = true;
+      lua_ls.enable = true;
+      marksman.enable = true;
+      nixd.enable = true;
+      pyright.enable = true;
+      terraformls.enable = true;
+      ts_ls.enable = true;
     };
     keymaps = {
       silent = true;
