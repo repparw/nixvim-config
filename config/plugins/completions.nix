3c963bbbcae6476a2e2cc4614dc987685ae99ea1
repparw <ubritos@gmail.com>
2026-03-17 15:35:35 -0300

    fix(cmp): copilot suggestions

--- config/plugins/completions.nix
+++ config/plugins/completions.nix
@@ -9,13 +9,32 @@
 
     blink-cmp = {
       enable = true;
-      settings.enabled = lib.generators.mkLuaInline ''
-        function()
-          return not vim.tbl_contains({ "markdown", "Avante" }, vim.bo.filetype)
-            and vim.bo.buftype ~= "prompt"
-            and vim.b.completion ~= false
-        end
-      '';
+      settings = {
+        enabled = lib.generators.mkLuaInline ''
+          function()
+            return not vim.tbl_contains({ "markdown", "Avante" }, vim.bo.filetype)
+              and vim.bo.buftype ~= "prompt"
+              and vim.b.completion ~= false
+          end
+        '';
+        sources = {
+          default = [
+            "lsp"
+            "path"
+            "snippets"
+            "buffer"
+            "blink-copilot"
+          ];
+          providers = {
+            blink-copilot = {
+              name = "Copilot";
+              module = "blink-copilot";
+              score_offset = 100;
+              async = true;
+            };
+          };
+        };
+      };
     };
   };
 }
