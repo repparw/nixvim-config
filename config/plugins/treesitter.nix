da43e9b168b896cbea11c4f0847d2992fcf353fb
repparw <ubritos@gmail.com>
2026-04-21 15:06:36 -0300

    plugins(treesitter): remove conditional enable, add grammarPackages

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -1,23 +1,25 @@
 {
-  pkgs,
-  lib,
+  config,
   ...
 }:
 {
   plugins.treesitter = {
     enable = true;
-    settings = {
-      highlight = {
-        enable = true;
-        disable = "function(lang, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end
-";
-      };
-    };
+    highlight.enable = true;
+
+    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+      bash
+      json
+      lua
+      make
+      markdown
+      nix
+      regex
+      toml
+      vim
+      vimdoc
+      xml
+      yaml
+    ];
   };
 }
